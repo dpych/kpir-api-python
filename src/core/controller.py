8477cd64ac6d446e643469f79a7417a2815356bb
Dawid Pych <dawidpych@wp.pl>
2015-07-23 23:12:38 +0200

    Controller core default response

--- src/core/controller.py
+++ src/core/controller.py
@@ -3,20 +3,31 @@ import core
 __author__ = "Dawid Pych <dawidpych@gmailcom>"
 __date__ = "$2015-07-05 15:32:20$"
 
+def response(response = {}, status=200, mimetype=None, page=1, lenght=1000):
+    if status == 200 :
+        success = True;
+    else:
+        success = False
 
-def response(response = {}, status=200, mimetype=None):
-    if not mimetype :
-        mimetype 
-    response = app.jsonify(response)
+    response = {
+        "data" : response,
+        "success" : success,
+        "page": page,
+        "length": lenght
+    }
+
+    response = core.jsonify(response)
     response.status_code = status
+
     return response
+
 
 def not_found(error=None):
     message = {
-            'status': 404,
-            'message': 'Not Found: ' + app.request.url,
+        'status': 404,
+        'message': 'Not Found: ' + core.request.url,
     }
-    resp = app.jsonify(message)
+    resp = core.jsonify(message)
     resp.status_code = 404
 
     return resp

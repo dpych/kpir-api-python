0708a097d045596ac64da2ec5da96d76eb2608e6
Dawid Pych <dawidpych@wp.pl>
2015-07-23 23:11:36 +0200

    Config for sqlite drive

--- src/core/config.py
+++ src/core/config.py
@@ -4,14 +4,25 @@ from core import os
 __author__="Dawid Pych <dawidpych@gmailcom>"
 __date__ ="$2015-07-05 12:34:07$"
 
-file = os.path.dirname(os.path.realpath(__file__)) + '/../config.ini'
+BASE_PATH = os.path.dirname(os.path.realpath(__file__)) + '/../'
+
+file = BASE_PATH + 'config.ini'
 
 Config = configparser.ConfigParser()
 Config.read(file)
 
 """
 Database connetion for mysql should looks like:
-    mysql+mysqldb://mydb_user:mydb_pwd@localhost:3306/mydb
+    For mysql: mysql+mysqldb://mydb_user:mydb_pwd@localhost:3306/mydb
+    For sqlite: sqlite:////abs/path/app.db
 """
-SQLALCHEMY_DATABASE_URI = Config.get('DATABASE', 'DRIVER') + '://' + Config.get('DATABASE', 'USER') + ':' + Config.get('DATABASE', 'PASSWD') + '@' + Config.get('DATABASE', 'HOST') + '/' + Config.get('DATABASE', 'BASE')
+if Config.get('DATABASE', 'DRIVER').lower() == 'sqlite' :
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BASE_PATH + 'app.db'
+else:
+    SQLALCHEMY_DATABASE_URI = Config.get('DATABASE', 'DRIVER') \
+        + '://' + Config.get('DATABASE', 'USER') \
+        + ':' + Config.get('DATABASE', 'PASSWD') \
+        + '@' + Config.get('DATABASE', 'HOST') \
+        + '/' + Config.get('DATABASE', 'BASE')
+
 SQLALCHEMY_MIGRATE_REPO = 'db_repository'

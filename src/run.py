8d012ad12f70c493b847c572c9a688d78c983390
Dawid Pych <dawidpych@wp.pl>
2015-07-23 23:15:24 +0200

    Remove not used imports

--- src/run.py
+++ src/run.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 from core import app
-#from app.products import controller
-#from app.clients import controller
 
 __author__ = "Dawid Pych <dawidpych@gmailcom>"
 __date__ = "$2015-07-05 11:42:32$"

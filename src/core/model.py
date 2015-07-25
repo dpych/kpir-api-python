69b5c616f5674ac64fa6d338f8ec36dcbbd70929
Dawid Pych <dawidpych@wp.pl>
2015-07-25 18:23:38 +0200

    kpir-api-python-2 Fix model after tests

--- src/core/model.py
+++ src/core/model.py
@@ -1,22 +1,25 @@
 from core import jsonify, db
 from sqlalchemy.ext.declarative import declared_attr
+from abc import ABCMeta
 
 __author__ = "Dawid Pych <dawidpych@gmailcom>"
 __date__ = "$2015-07-05 14:07:00$"
 
 """
-Defauld model for all new models in app 
+Default model for all new models in app
 """
 
+
 class CoreModel (object):
-    id =  db.Column(db.Integer, primary_key=True)
-    params = db.Column(db.Text)
+    __metaclass__ = ABCMeta
     __table__ = None
-    
+
+    id = db.Column(db.Integer, primary_key=True)
+    params = db.Column(db.Text)
+
     def __init__(self):
-        super(self)
-        self.__table__ = cls.__name__.lower()
-    
+        self.__table__ = self.__name__.lower()
+
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()

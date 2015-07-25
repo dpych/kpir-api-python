96f3baac4e5b33580475048b8da49a50ee4f5feb
Dawid Pych <dawidpych@wp.pl>
2015-07-25 16:25:52 +0200

    kpir-api-python-2 Added tests

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Dawid Pych'
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
---
+++ test/test_core_config.py
@@ -0,0 +1,34 @@
+import core
+from flask.ext.testing import TestCase
+
+__author__ = 'Dawid Pych <dawidpych@gmail.com>'
+__date__ = '19.07.15'
+
+
+class MyTestCase(TestCase):
+
+    def create_app(self):
+        return core.app
+
+    def test_config_file_url_exists(self):
+        url = core.config.file
+        self.assertIsNotNone(url)
+
+    def test_config_file_exists(self):
+        url = core.config.file
+        self.assertTrue(core.os.path.isfile(url))
+
+    def test_database_uri(self):
+        uri = core.SQLALCHEMY_DATABASE_URI
+        self.assertIsNotNone(uri)
+
+    def test_database_migrate_dir(self):
+        dir = core.SQLALCHEMY_MIGRATE_REPO
+        self.assertIsNotNone(dir)
+
+    def test_is_sqllite_file_url(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

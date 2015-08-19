d2f3cc93144d964aadc03b730dcd09dd4fc09dd2
Dawid Pych <dawidpych@wp.pl>
2015-08-19 15:48:21 +0200

    #12 new options

--- src/run.py
+++ src/run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from kpir.core import app
+from kpir.core import app, Config
 
 __author__ = "Dawid Pych <dawidpych@gmailcom>"
 __date__ = "$2015-07-05 11:42:32$"
 
 if __name__ == "__main__":
-    app.run(debug=True);
+    app.run(debug=Config.get('RUN', 'DEBUG'), host=Config.get('RUN', 'HOST'))
